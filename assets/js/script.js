4ca8fc958ab1f1626f2ee4303bda7bb2ad39a3d4
Dérik Figueiredo <derikdeewk@gmail.com>
2021-04-12 14:26:17 -0300

    Responsividade

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.menuMobile').addEventListener('click', ()=>{
+    if (document.querySelector('.menu nav ul').style.display == 'flex') {
+    document.querySelector('.menu nav ul').style.display = "none";
+    } else {
+        document.querySelector('.menu nav ul').style.display = "flex";
+    }
+})
